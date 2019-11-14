26f33724053d1278cd12b8dc04d308d8cceb29a1
dakidron <dakidron@B105-52-90>
2019-11-14 14:58:54 +0200

    aaaaa

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello");
+            Console.WriteLine("12345");
         }
     }
 }
